4fd5cbd840ff012a7cd552d0835d714d44d60c56
erick1345 <erickc.jj1@gmail.com>
2026-07-02 11:35:41 -0300

    feat: site apenas com a landing page; menu do topo navega por secoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,5 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { ProtectedRoute } from './components/ProtectedRoute';
 import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import ServicosList from './pages/servicos/ServicosList';
-import ServicoForm from './pages/servicos/ServicoForm';
-import ProdutosList from './pages/produtos/ProdutosList';
-import ProdutoForm from './pages/produtos/ProdutoForm';
-import AgendamentosList from './pages/agendamentos/AgendamentosList';
-import AgendamentoForm from './pages/agendamentos/AgendamentoForm';
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/cadastro" element={<Register />} />
-      <Route element={<ProtectedRoute />}>
-        <Route path="/painel" element={<Home />} />
-        <Route path="/perfil" element={<Profile />} />
-        <Route path="/servicos" element={<ServicosList />} />
-        <Route path="/servicos/novo" element={<ServicoForm />} />
-        <Route path="/servicos/:id/editar" element={<ServicoForm />} />
-        <Route path="/produtos" element={<ProdutosList />} />
-        <Route path="/produtos/novo" element={<ProdutoForm />} />
-        <Route path="/produtos/:id/editar" element={<ProdutoForm />} />
-        <Route path="/agendamentos" element={<AgendamentosList />} />
-        <Route path="/agendamentos/novo" element={<AgendamentoForm />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+  return <Landing />;
 }
